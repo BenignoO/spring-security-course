ebd1b2dcefe038fc2abbe73b2c8e26e14470472c
Benigno Orellana <jbenigno.2993@gmail.com>
2023-09-06 16:56:29 -0600

    Tercer Commit

--- src/main/java/com/cursos/springsecuritycourse/dto/AuthenticationRequest.java
+++ src/main/java/com/cursos/springsecuritycourse/dto/AuthenticationRequest.java
@@ -1,10 +1,14 @@
 package com.cursos.springsecuritycourse.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationRequest {
     private String username;
     private String password;
